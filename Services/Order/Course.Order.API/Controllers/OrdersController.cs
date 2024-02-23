d42ffc8138d16f2fe3a94bb275ebb0804a9d58af
batuhanb <balibatuhan19@gmail.com>
2024-02-23 17:00:58 +0300

    Orders Controller has completed

--- Services/Order/Course.Order.API/Controllers/OrdersController.cs
+++ Services/Order/Course.Order.API/Controllers/OrdersController.cs
@@ -1,12 +1,32 @@
-﻿using Course.Shared.BaseController;
+﻿using Course.Order.Application.Features.Orders.Commands.CreateOrder;
+using Course.Order.Application.Features.Orders.Queries.GetOrdersByUserId;
+using Course.Shared.BaseController;
+using Course.Shared.Services;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Course.Order.API.Controllers;
-public class OrdersController : BaseController
+
+public class OrdersController(ISharedIdentityService identityService) : BaseController
 {
+    private readonly ISharedIdentityService _identityService = identityService;
+    private IMediator _mediator;
+
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
     [HttpGet]
-    public string Get()
+    public async Task<IActionResult> Get(int page, int pageSize,CancellationToken cancellationToken = default)
     {
-        return "a";
+        var result = await Mediator.Send(new GetOrdersByUserIdQuery(_identityService.GetUserId, new(page, pageSize)), cancellationToken));
+
+        return CreateActionResultInstance(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateOrderCommand command, CancellationToken cancellationToken = default)
+    {
+        var result = await Mediator.Send(command, cancellationToken);
+
+        return CreateActionResultInstance(result);
     }
 }
